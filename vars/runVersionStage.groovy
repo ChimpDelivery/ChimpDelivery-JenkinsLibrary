d5bd6f8195159e439a1c40bbb1d10fbf7c926cd7
Emre Kovanci <kovanciemre@gmail.com>
2022-12-05 17:29:53 +0300

    Create runVersionStage.groovy

---
+++ vars/runVersionStage.groovy
@@ -0,0 +1,25 @@
+def call() 
+{
+    sh '''
+        echo "[Jenkins] Unity Set Product Settings stage starting..."
+        echo "[Jenkins] BuildNumber: ${BUILD_NUMBER}, AppVersion: ${STORE_BUILD_VERSION}, CustomVersion: ${STORE_CUSTOM_BUNDLE_VERSION}, CustomBundleVersion: ${STORE_BUNDLE_VERSION}"
+
+        $UNITY_PATH -batchmode \
+                    -projectPath $WORKSPACE \
+                    -executeMethod $UNITY_ASSET_METHOD \
+                    -username $UNITY_USERNAME \
+                    -password $UNITY_PASSWORD \
+                    -serial $UNITY_SERIAL \
+                    -apiKey $BACKEND_API_KEY \
+                    -apiUrl $BACKEND_API_URL \
+                    -appId $APP_ID \
+                    -buildVersion $STORE_BUILD_VERSION \
+                    -bundleVersion $STORE_VERSION \
+                    -profileUuid $DASHBOARD_PROFILE_UUID \
+                    -nographics \
+                    -stackTraceLogType Full \
+                    -silent-crashes
+
+                    echo "[Jenkins] Unity Set Product Settings stage completed!"
+    '''
+}
