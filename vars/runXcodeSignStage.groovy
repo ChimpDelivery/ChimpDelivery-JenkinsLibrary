2cfa5e10bb5bab4ca1e0e238e0e1976104f23d45
Emre Kovanci <kovanciemre@gmail.com>
2022-12-05 18:08:54 +0300

    Update runXcodeSignStage.groovy

--- vars/runXcodeSignStage.groovy
+++ vars/runXcodeSignStage.groovy
@@ -1,4 +1,6 @@
 def call()
 {
+    env.LIBRARY_ROOT = "/Volumes/Mac/Users/emrekovanci"
+    env.PROVISION_LIBRARY_PATH = "$LIBRARY_ROOT/Library/MobileDevice/Provisioning Profiles"
     sh '''cp "$WORKSPACE/Builds/temp-provision" "$PROVISION_LIBRARY_PATH/$DASHBOARD_PROFILE_UUID.mobileprovision"'''
 }
