3c8e90bc7ba58f8aabe2a0194e5762b9bc19e325
Emre Kovanci <kovanciemre@gmail.com>
2022-12-05 18:14:58 +0300

    Update runXcodeArchiveStage.groovy

--- vars/runXcodeArchiveStage.groovy
+++ vars/runXcodeArchiveStage.groovy
@@ -1,5 +1,8 @@
 def call()
 {
+    env.IS_WORKSPACE = fileExists("${WORKSPACE}/Builds/UnityBuild/Unity-iPhone.xcworkspace")
+    env.XCODE_PROJECT = "${IS_WORKSPACE == "true" ? "only_active_arch=yes -workspace Unity-iPhone.xcworkspace" : "-project Unity-iPhone.xcodeproj"}"
+    
     sh '''
         echo "[Jenkins] Create Archive starting..."
         echo "[Jenkins] IS_WORKSPACE: $IS_WORKSPACE, XCODE_PROJECT_ARGUMENT: $XCODE_PROJECT"
