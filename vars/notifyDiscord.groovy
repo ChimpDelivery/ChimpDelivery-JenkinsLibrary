a0b437dbdfcb945ab354cb5f49300fc0f47a31c8
Emre Kovanci <kovanciemre@gmail.com>
2022-12-05 18:28:46 +0300

    Update notifyDiscord.groovy

--- vars/notifyDiscord.groovy
+++ vars/notifyDiscord.groovy
@@ -1,9 +1,9 @@
-def call() 
+def call(Map config = [:]) 
 {
     discordSend description: "Build ${BUILD_NUMBER}: ${currentBuild.currentResult}",
                 footer: "Platform: ${PLATFORM}, Version: ${STORE_BUILD_VERSION} / ${STORE_VERSION}, App ID: ${APP_ID}",
                 link: env.BUILD_URL,
                 result: currentBuild.currentResult,
                 title: "Job: ${JOB_NAME}",
-                webhookURL: env.DISCORD_WEBHOOK_URL
+                webhookURL: ${config.DISCORD_WEBHOOK_URL}
 }
