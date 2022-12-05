fc87d07a05da79385626e73c31c714b0ab0dba32
Emre Kovanci <kovanciemre@gmail.com>
2022-12-05 17:22:53 +0300

    Create clearProjectArtifacts.groovy

---
+++ vars/clearProjectArtifacts.groovy
@@ -0,0 +1,26 @@
+def call() 
+{
+    // clear build related artifacts
+    sh '''
+        if [ -d "${WORKSPACE}/Builds" ]; then
+            rm -rf "${WORKSPACE}/Builds"
+        fi
+    '''
+
+    // clear project related artifacts
+    if (params.INSTALL_SDK == true)
+    {
+        sh '''
+            git checkout "${WORKSPACE}/Packages/manifest.json"
+            git checkout "${WORKSPACE}/Packages/packages-lock.json"
+
+            if [ -d "${WORKSPACE}/Assets/FacebookSDK" ] then
+                rm -rf "${WORKSPACE}/Assets/FacebookSDK"
+            fi
+
+            if [ -d "${WORKSPACE}/Assets/Plugins/Android" ]; then
+                rm -rf "${WORKSPACE}/Assets/Plugins/Android"
+            fi
+        '''
+    }
+}
