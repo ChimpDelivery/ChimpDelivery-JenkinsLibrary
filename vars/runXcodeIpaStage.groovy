f611c5d57054688d034b277cf30f43e39ed6e644
Emre Kovanci <kovanciemre@gmail.com>
2022-12-05 17:47:36 +0300

    Create runXcodeIpaStage.groovy

---
+++ vars/runXcodeIpaStage.groovy
@@ -0,0 +1,15 @@
+def call()
+{
+    sh '''
+        echo "[Jenkins] Create ipa starting..."
+
+        cd $WORKSPACE/Builds/UnityBuild
+
+        /usr/bin/xcodebuild -exportArchive \
+            -archivePath '../ios-build/BuildArchieve.xcarchive' \
+            -exportPath '.' -exportOptionsPlist $EXPORTLIST \
+            -verbose
+
+        echo "[Jenkins] Create ipa finished..."
+    '''
+}
