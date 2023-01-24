622b4394cd09b2a489b08fec13e90d02d239d28d
Emre Kovanci <kovanciemre@gmail.com>
2023-01-24 20:09:52 +0300

    Update clearProjectArtifacts.groovy

--- vars/clearProjectArtifacts.groovy
+++ vars/clearProjectArtifacts.groovy
@@ -1,26 +1,29 @@
 def call() 
 {
-    // clear build related artifacts
-    sh '''
-        if [ -d "${WORKSPACE}/Builds" ]; then
-            rm -rf "${WORKSPACE}/Builds"
-        fi
-    '''
-
-    // clear project related artifacts
-    if (params.INSTALL_SDK == true)
+    node
     {
+        // clear build related artifacts
         sh '''
-            git checkout "${WORKSPACE}/Packages/manifest.json"
-            git checkout "${WORKSPACE}/Packages/packages-lock.json"
-
-            if [ -d "${WORKSPACE}/Assets/FacebookSDK" ]; then
-                rm -rf "${WORKSPACE}/Assets/FacebookSDK"
-            fi
-
-            if [ -d "${WORKSPACE}/Assets/Plugins/Android" ]; then
-                rm -rf "${WORKSPACE}/Assets/Plugins/Android"
+            if [ -d "${WORKSPACE}/Builds" ]; then
+                rm -rf "${WORKSPACE}/Builds"
             fi
         '''
+
+        // clear project related artifacts
+        if (params.INSTALL_SDK == true)
+        {
+            sh '''
+                git checkout "${WORKSPACE}/Packages/manifest.json"
+                git checkout "${WORKSPACE}/Packages/packages-lock.json"
+
+                if [ -d "${WORKSPACE}/Assets/FacebookSDK" ]; then
+                    rm -rf "${WORKSPACE}/Assets/FacebookSDK"
+                fi
+
+                if [ -d "${WORKSPACE}/Assets/Plugins/Android" ]; then
+                    rm -rf "${WORKSPACE}/Assets/Plugins/Android"
+                fi
+            '''
+        }
     }
 }
