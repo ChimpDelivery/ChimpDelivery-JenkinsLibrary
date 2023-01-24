544ddd157435f8937b15b089ffc2a3376f76a2bd
Emre Kovanci <kovanciemre@gmail.com>
2023-01-24 20:18:11 +0300

    Update notifyDiscord.groovy

--- vars/notifyDiscord.groovy
+++ vars/notifyDiscord.groovy
@@ -1,12 +1,9 @@
 def call() 
 {
-    node
-    {
-        discordSend description: "Build ${BUILD_NUMBER}: ${currentBuild.currentResult}",
-                    footer: "Platform: ${PLATFORM}, Version: ${STORE_BUILD_VERSION} / xxx, App ID: ${APP_ID}",
-                    link: env.BUILD_URL,
-                    result: currentBuild.currentResult,
-                    title: "Job: ${JOB_NAME}",
-                    webhookURL: env.DISCORD_WEBHOOK_URL
-    }
+    discordSend description: "Build ${BUILD_NUMBER}: ${currentBuild.currentResult}",
+                footer: "Platform: ${PLATFORM}, Version: ${STORE_BUILD_VERSION} / ${STORE_VERSION}, App ID: ${APP_ID}",
+                link: env.BUILD_URL,
+                result: currentBuild.currentResult,
+                title: "Job: ${JOB_NAME}",
+                webhookURL: env.DISCORD_WEBHOOK_URL
 }
