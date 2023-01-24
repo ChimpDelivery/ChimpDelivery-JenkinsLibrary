83da4368896af8cf246f5ef837be02deed4ac729
Emre Kovanci <kovanciemre@gmail.com>
2023-01-24 20:14:59 +0300

    Update notifyDiscord.groovy

--- vars/notifyDiscord.groovy
+++ vars/notifyDiscord.groovy
@@ -3,7 +3,7 @@ def call()
     node
     {
         discordSend description: "Build ${BUILD_NUMBER}: ${currentBuild.currentResult}",
-                    footer: "Platform: ${PLATFORM}, Version: ${STORE_BUILD_VERSION} / ${STORE_VERSION}, App ID: ${APP_ID}",
+                    footer: "Platform: ${PLATFORM}, Version: ${STORE_BUILD_VERSION} / ${env.STORE_VERSION}, App ID: ${APP_ID}",
                     link: env.BUILD_URL,
                     result: currentBuild.currentResult,
                     title: "Job: ${JOB_NAME}",
