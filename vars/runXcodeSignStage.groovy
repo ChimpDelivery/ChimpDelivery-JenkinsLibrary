ac7b9d4145387814b2b2906fe60f4141c04092a5
Emre Kovanci <kovanciemre@gmail.com>
2023-01-24 19:58:12 +0300

    Update runXcodeSignStage.groovy

--- vars/runXcodeSignStage.groovy
+++ vars/runXcodeSignStage.groovy
@@ -1,8 +1,6 @@
-import java.nio.file.Paths
-
 def call()
 {
     env.LIBRARY_ROOT = "/Volumes/Mac/$HOME"
-    env.PROVISION_LIBRARY_PATH = Paths.get("$LIBRARY_ROOT", "/Library/MobileDevice/Provisioning Profiles")
+    env.PROVISION_LIBRARY_PATH = "$LIBRARY_ROOT/Library/MobileDevice/Provisioning Profiles"
     sh '''cp "$WORKSPACE/Builds/temp-provision" "$PROVISION_LIBRARY_PATH/$DASHBOARD_PROFILE_UUID.mobileprovision"'''
 }
