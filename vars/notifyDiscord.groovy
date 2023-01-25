2ffb9af6eef73e222479b44e56a889fdc3c93fa5
Emre Kovanci <kovanciemre@gmail.com>
2023-01-25 07:46:47 +0300

    Update notifyDiscord.groovy

--- vars/notifyDiscord.groovy
+++ vars/notifyDiscord.groovy
@@ -1,6 +1,5 @@
 def call() 
 {
-    return ""
     discordSend description: "Build ${BUILD_NUMBER}: ${currentBuild.currentResult}",
                 footer: "Platform: ${PLATFORM}, Version: ${STORE_BUILD_VERSION} / ${STORE_VERSION}, App ID: ${APP_ID}",
                 link: env.BUILD_URL,
