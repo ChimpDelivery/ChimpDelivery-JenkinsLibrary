c4665542bb72e930496a7491fc4831041a882f24
Emre Kovanci <kovanciemre@gmail.com>
2023-01-30 01:40:25 +0300

    Update unlockKeychain.groovy

--- vars/unlockKeychain.groovy
+++ vars/unlockKeychain.groovy
@@ -1,4 +1,7 @@
 def call() 
 {
-    sh '''security unlock-keychain -p $KEYCHAIN_PASS $KEYCHAIN_LOCATION'''
+    // security import "${IOS_P12_FILE_PATH}" -f pkcs12 -P "${iOS_P12_PASSWORD}" -k "${KEYCHAIN_LOCATION}" -T /usr/bin/codesign
+    sh '''
+        security unlock-keychain -p $KEYCHAIN_PASS $KEYCHAIN_LOCATION
+   '''
 }
