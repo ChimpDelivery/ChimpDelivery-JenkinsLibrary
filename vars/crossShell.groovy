aa0a77982bb34baa126f60a35574a8a574225f6e
Emre Kovanci <kovanciemre@gmail.com>
2023-03-28 13:15:51 +0300

    Update crossShell.groovy

--- vars/crossShell.groovy
+++ vars/crossShell.groovy
@@ -2,8 +2,8 @@ def call(command)
 {
     if (isUnix())
     {
-        sh(script: command, encoding: 'UTF-8', returnStatus: true, returnStdout: true)
+        return sh(script: command, encoding: 'UTF-8', returnStatus: true, returnStdout: true)
     }
     
-    bat(script: command, encoding: 'UTF-8', returnStatus: true, returnStdout: true)
+    return bat(script: command, encoding: 'UTF-8', returnStatus: true, returnStdout: true)
 }
