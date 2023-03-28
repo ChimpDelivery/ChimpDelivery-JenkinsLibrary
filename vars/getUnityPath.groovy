ce6967bbe53353e868a9b792ba359bf07bb4d5e9
Emre Kovanci <kovanciemre@gmail.com>
2023-03-28 15:51:35 +0300

    Update getUnityPath.groovy

--- vars/getUnityPath.groovy
+++ vars/getUnityPath.groovy
@@ -1,13 +1,13 @@
-def call() 
+def call(String unityVersion) 
 {
     switch (env.OS)
     {
         case 'WIN':
-            return "C:/Program Files/Unity/Hub/Editor/${params.UNITY_VERSION}/Editor/Unity.exe";
+            return "C:/Program Files/Unity/Hub/Editor/${unityVersion}/Editor/Unity.exe";
         break;
         
         case 'MAC':
-            return "/Applications/Unity/Hub/Editor/${params.UNITY_VERSION}/Unity.app/Contents/MacOS/Unity";
+            return "/Applications/Unity/Hub/Editor/${unityVersion}/Unity.app/Contents/MacOS/Unity";
         break;
         
         case 'UBUNTU':
