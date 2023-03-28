97bd8109548f25b532f51a0d1ad48fcc0a09b590
Emre Kovanci <kovanciemre@gmail.com>
2023-03-28 15:10:30 +0300

    Update runVersionStage.groovy

--- vars/runVersionStage.groovy
+++ vars/runVersionStage.groovy
@@ -5,24 +5,24 @@ def call()
     crossShell """
         echo "[Jenkins] Unity Set Product Settings stage starting..."
         
-        echo "[Jenkins] BuildNumber: "${BUILD_NUMBER}" "${MULTI_LINE_SEPERATOR}"
-                        AppVersion: "${STORE_BUILD_VERSION}" "${MULTI_LINE_SEPERATOR}"
-                        CustomVersion: "${STORE_CUSTOM_BUNDLE_VERSION}" "${MULTI_LINE_SEPERATOR}"
-                        CustomBundleVersion: "${STORE_BUNDLE_VERSION}" "
+        echo "[Jenkins] BuildNumber: ${BUILD_NUMBER} ${MULTI_LINE_SEPERATOR}
+                        AppVersion: ${STORE_BUILD_VERSION} ${MULTI_LINE_SEPERATOR}
+                        CustomVersion: ${STORE_CUSTOM_BUNDLE_VERSION} ${MULTI_LINE_SEPERATOR}
+                        CustomBundleVersion: ${STORE_BUNDLE_VERSION}"
 
-        "${UNITY_PATH}" -batchmode "${MULTI_LINE_SEPERATOR}"
-                    -projectPath "${WORKSPACE}" "${MULTI_LINE_SEPERATOR}"
-                    -executeMethod "${UNITY_ASSET_METHOD}" "${MULTI_LINE_SEPERATOR}"
-                    -username "${UNITY_USERNAME}" "${MULTI_LINE_SEPERATOR}"
-                    -password "${UNITY_PASSWORD}" "${MULTI_LINE_SEPERATOR}"
-                    -serial "${UNITY_SERIAL}" "${MULTI_LINE_SEPERATOR}"
-                    -apiUrl "${DASHBOARD_ACCOUNT_USR}" "${MULTI_LINE_SEPERATOR}"R
-                    -apiKey "${DASHBOARD_ACCOUNT_PSW}" "${MULTI_LINE_SEPERATOR}"
-                    -appId "${APP_ID}" $MULTI_LINE_SEPERATOR
-                    -buildVersion "${STORE_BUILD_VERSION}" "${MULTI_LINE_SEPERATOR}"
-                    -bundleVersion "${STORE_VERSION}" "${MULTI_LINE_SEPERATOR}"
-                    -nographics "${MULTI_LINE_SEPERATOR}"
-                    -stackTraceLogType ScriptOnly "${MULTI_LINE_SEPERATOR}"
+        "${UNITY_PATH}" -batchmode ${MULTI_LINE_SEPERATOR}
+                    -projectPath "${WORKSPACE}" ${MULTI_LINE_SEPERATOR}
+                    -executeMethod "${UNITY_ASSET_METHOD}" ${MULTI_LINE_SEPERATOR}
+                    -username "${UNITY_USERNAME}" ${MULTI_LINE_SEPERATOR}
+                    -password "${UNITY_PASSWORD}" ${MULTI_LINE_SEPERATOR}
+                    -serial "${UNITY_SERIAL}" ${MULTI_LINE_SEPERATOR}
+                    -apiUrl "${DASHBOARD_ACCOUNT_USR}" ${MULTI_LINE_SEPERATOR}
+                    -apiKey "${DASHBOARD_ACCOUNT_PSW}" "${MULTI_LINE_SEPERATOR}
+                    -appId "${APP_ID}" ${MULTI_LINE_SEPERATOR}
+                    -buildVersion "${STORE_BUILD_VERSION}" ${MULTI_LINE_SEPERATOR}
+                    -bundleVersion "${STORE_VERSION}" ${MULTI_LINE_SEPERATOR}
+                    -nographics ${MULTI_LINE_SEPERATOR}
+                    -stackTraceLogType ScriptOnly ${MULTI_LINE_SEPERATOR}
                     -silent-crashes
                     
         echo "[Jenkins] Unity Set Product Settings stage completed!"
