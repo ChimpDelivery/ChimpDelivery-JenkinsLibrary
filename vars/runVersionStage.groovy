03b5ab92475fc547a95b9d805c934d2a477837ba
Emre Kovanci <kovanciemre@gmail.com>
2023-03-28 14:42:21 +0300

    Update runVersionStage.groovy

--- vars/runVersionStage.groovy
+++ vars/runVersionStage.groovy
@@ -1,26 +1,28 @@
 def call() 
 {
+    String multiLineSeperator = getCrossMultiLine()
+    
     crossShell """
         echo '[Jenkins] Unity Set Product Settings stage starting...'
         
-        echo '[Jenkins] BuildNumber: $BUILD_NUMBER \
-                        AppVersion: $STORE_BUILD_VERSION \
-                        CustomVersion: $STORE_CUSTOM_BUNDLE_VERSION \
+        echo '[Jenkins] BuildNumber: $BUILD_NUMBER multiLineSeperator
+                        AppVersion: $STORE_BUILD_VERSION multiLineSeperator
+                        CustomVersion: $STORE_CUSTOM_BUNDLE_VERSION multiLineSeperator
                         CustomBundleVersion: $STORE_BUNDLE_VERSION'
 
-        "${UNITY_PATH}" -batchmode \
-                    -projectPath $WORKSPACE \
-                    -executeMethod $UNITY_ASSET_METHOD \
-                    -username $UNITY_USERNAME \
-                    -password $UNITY_PASSWORD \
-                    -serial $UNITY_SERIAL \
-                    -apiUrl $DASHBOARD_ACCOUNT_USR \
-                    -apiKey $DASHBOARD_ACCOUNT_PSW \
-                    -appId $APP_ID \
-                    -buildVersion $STORE_BUILD_VERSION \
-                    -bundleVersion $STORE_VERSION \
-                    -nographics \
-                    -stackTraceLogType ScriptOnly \
+        "${UNITY_PATH}" -batchmode multiLineSeperator
+                    -projectPath $WORKSPACE multiLineSeperator
+                    -executeMethod $UNITY_ASSET_METHOD multiLineSeperator
+                    -username $UNITY_USERNAME multiLineSeperator
+                    -password $UNITY_PASSWORD multiLineSeperator
+                    -serial $UNITY_SERIAL multiLineSeperator
+                    -apiUrl $DASHBOARD_ACCOUNT_USR multiLineSeperator
+                    -apiKey $DASHBOARD_ACCOUNT_PSW multiLineSeperator
+                    -appId $APP_ID multiLineSeperator
+                    -buildVersion $STORE_BUILD_VERSION multiLineSeperator
+                    -bundleVersion $STORE_VERSION multiLineSeperator
+                    -nographics multiLineSeperator
+                    -stackTraceLogType ScriptOnly multiLineSeperator
                     -silent-crashes
                     
         echo '[Jenkins] Unity Set Product Settings stage completed!'
