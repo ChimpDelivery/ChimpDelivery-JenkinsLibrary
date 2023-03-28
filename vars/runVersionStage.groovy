d87fbdeeb19069f885f6077acd9247489cf72e52
Emre Kovanci <kovanciemre@gmail.com>
2023-03-28 17:45:28 +0300

    Update runVersionStage.groovy

--- vars/runVersionStage.groovy
+++ vars/runVersionStage.groovy
@@ -3,22 +3,7 @@ def call()
     crossShell(script: """ echo [Jenkins] Unity Set Product Settings stage starting... """)
     crossShell(script: """ echo [Jenkins] BuildNumber: "$BUILD_NUMBER", AppVersion: "$STORE_BUILD_VERSION", CustomVersion: "$STORE_CUSTOM_BUNDLE_VERSION", CustomBundleVersion: "$STORE_BUNDLE_VERSION" """)           
     
-    sh '''
-        $UNITY_PATH -batchmode \
-                    -projectPath $WORKSPACE \
-                    -executeMethod $UNITY_ASSET_METHOD \
-                    -username $UNITY_USERNAME \
-                    -password $UNITY_PASSWORD \
-                    -serial $UNITY_SERIAL \
-                    -apiUrl $DASHBOARD_ACCOUNT_USR \
-                    -apiKey $DASHBOARD_ACCOUNT_PSW \
-                    -appId $APP_ID \
-                    -buildVersion $STORE_BUILD_VERSION \
-                    -bundleVersion $STORE_VERSION \
-                    -nographics \
-                    -stackTraceLogType ScriptOnly \
-                    -silent-crashes
-                    
-        echo "[Jenkins] Unity Set Product Settings stage completed!"
-    '''
+    crossShell(script: """ $UNITY_PATH -batchmode -projectPath $WORKSPACE -executeMethod $UNITY_ASSET_METHOD -username $UNITY_USERNAME -password $UNITY_PASSWORD -serial $UNITY_SERIAL -apiUrl $DASHBOARD_ACCOUNT_USR -apiKey $DASHBOARD_ACCOUNT_PSW -appId $APP_ID -buildVersion $STORE_BUILD_VERSION -bundleVersion $STORE_VERSION -nographics -stackTraceLogType ScriptOnly -silent-crashes""")
+    
+    crossShell(script: """ echo [Jenkins] Unity Set Product Settings stage completed! """)
 }
