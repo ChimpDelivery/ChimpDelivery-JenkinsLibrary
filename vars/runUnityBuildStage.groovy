e60c4b45dc9bc66dba58ace629a97cc3e537516c
Emre Kovanci <kovanciemre@gmail.com>
2023-03-29 17:13:27 +0300

    Update runUnityBuildStage.groovy

--- vars/runUnityBuildStage.groovy
+++ vars/runUnityBuildStage.groovy
@@ -1,30 +1,57 @@
 def call()
 {
-    sh '''
-        echo "[Jenkins] Unity Build starting..."
+    if (isUnix())
+    {
+        sh '''
+            echo "[Jenkins] Unity Build starting..."
 
-        export LANG=en_US.UTF-8
+            export LANG=en_US.UTF-8
 
-        echo "[Jenkins] Job Environment Variables: \
-            PLATFORM: $PLATFORM \
-            APP_ID: $APP_ID \
-            BUILD_METHOD: $UNITY_BUILD_METHOD"
+            echo "[Jenkins] Job Environment Variables: \
+                PLATFORM: $PLATFORM \
+                APP_ID: $APP_ID \
+                BUILD_METHOD: $UNITY_BUILD_METHOD"
 
-        $UNITY_PATH -batchmode \
-            -projectPath $WORKSPACE \
-            -executeMethod $UNITY_BUILD_METHOD \
-            -username $UNITY_USERNAME \
-            -password $UNITY_PASSWORD \
-            -serial $UNITY_SERIAL \
-            -apiUrl $DASHBOARD_ACCOUNT_USR \
-            -apiKey $DASHBOARD_ACCOUNT_PSW \
-            -appId $APP_ID \
-            -keyStorePass $ANDROID_KEYSTORE_PASS \
-            -nographics \
-            -accept-apiupdate \
-            -stackTraceLogType ScriptOnly \
-            -silent-crashes
+            $UNITY_PATH -batchmode \
+                -projectPath $WORKSPACE \
+                -executeMethod $UNITY_BUILD_METHOD \
+                -username $UNITY_USERNAME \
+                -password $UNITY_PASSWORD \
+                -serial $UNITY_SERIAL \
+                -apiUrl $DASHBOARD_ACCOUNT_USR \
+                -apiKey $DASHBOARD_ACCOUNT_PSW \
+                -appId $APP_ID \
+                -keyStorePass $ANDROID_KEYSTORE_PASS \
+                -nographics \
+                -accept-apiupdate \
+                -stackTraceLogType ScriptOnly \
+                -silent-crashes
 
-            echo "[Jenkins] Unity Build finished..."
-        '''
+                echo "[Jenkins] Unity Build finished..."
+            '''
+    }
+    else
+    {
+        bat '''
+            echo "[Jenkins] Unity Build starting..."
+            echo "[Jenkins] Job Environment Variables: PLATFORM: $PLATFORM, APP_ID: $APP_ID, BUILD_METHOD: $UNITY_BUILD_METHOD"
+
+            "$UNITY_PATH" -batchmode ^
+                -projectPath "$WORKSPACE" ^
+                -executeMethod "$UNITY_BUILD_METHOD" ^
+                -username "$UNITY_USERNAME" ^
+                -password "$UNITY_PASSWORD" ^
+                -serial "$UNITY_SERIAL" ^
+                -apiUrl "$DASHBOARD_ACCOUNT_USR" ^
+                -apiKey "$DASHBOARD_ACCOUNT_PSW" ^
+                -appId "$APP_ID" ^
+                -keyStorePass "$ANDROID_KEYSTORE_PASS" ^
+                -nographics ^
+                -accept-apiupdate ^
+                -stackTraceLogType ScriptOnly ^
+                -silent-crashes
+
+                echo "[Jenkins] Unity Build finished..."
+            '''
+    }
 }
