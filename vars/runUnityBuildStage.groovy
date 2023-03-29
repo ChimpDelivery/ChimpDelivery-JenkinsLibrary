9710d6499fd8604292711db07851f26996395d8c
Emre Kovanci <kovanciemre@gmail.com>
2023-03-29 17:16:41 +0300

    Update runUnityBuildStage.groovy

--- vars/runUnityBuildStage.groovy
+++ vars/runUnityBuildStage.groovy
@@ -34,18 +34,18 @@ def call()
     {
         bat '''
             echo "[Jenkins] Unity Build starting..."
-            echo "[Jenkins] Job Environment Variables: PLATFORM: $PLATFORM, APP_ID: $APP_ID, BUILD_METHOD: $UNITY_BUILD_METHOD"
+            echo "[Jenkins] Job Environment Variables: PLATFORM: %PLATFORM%, APP_ID: %APP_ID%, BUILD_METHOD: %UNITY_BUILD_METHOD%"
 
-            "$UNITY_PATH" -batchmode ^
-                -projectPath "$WORKSPACE" ^
-                -executeMethod "$UNITY_BUILD_METHOD" ^
-                -username "$UNITY_USERNAME" ^
-                -password "$UNITY_PASSWORD" ^
-                -serial "$UNITY_SERIAL" ^
-                -apiUrl "$DASHBOARD_ACCOUNT_USR" ^
-                -apiKey "$DASHBOARD_ACCOUNT_PSW" ^
-                -appId "$APP_ID" ^
-                -keyStorePass "$ANDROID_KEYSTORE_PASS" ^
+            "%UNITY_PATH%" -batchmode ^
+                -projectPath "%WORKSPACE%" ^
+                -executeMethod "%UNITY_BUILD_METHOD%" ^
+                -username "%UNITY_USERNAME%" ^
+                -password "%UNITY_PASSWORD%" ^
+                -serial "%UNITY_SERIAL%" ^
+                -apiUrl "%DASHBOARD_ACCOUNT_USR%" ^
+                -apiKey "%$DASHBOARD_ACCOUNT_PSW%" ^
+                -appId "%APP_ID%" ^
+                -keyStorePass "%ANDROID_KEYSTORE_PASS%" ^
                 -nographics ^
                 -accept-apiupdate ^
                 -stackTraceLogType ScriptOnly ^
