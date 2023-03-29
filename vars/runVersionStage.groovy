b2aac156f30af231697d13a196c3390515f7fdf6
Emre Kovanci <kovanciemre@gmail.com>
2023-03-29 15:23:30 +0300

    Update runVersionStage.groovy

--- vars/runVersionStage.groovy
+++ vars/runVersionStage.groovy
@@ -30,8 +30,8 @@ def call()
             echo "[Jenkins] Unity Set Product Settings stage starting..."
             echo "[Jenkins] BuildNumber: %BUILD_NUMBER%, AppVersion: %STORE_BUILD_VERSION%, CustomVersion: %STORE_CUSTOM_BUNDLE_VERSION%, CustomBundleVersion: %STORE_BUNDLE_VERSION%"
 
-            "%UNITY_PATH%" -batchmode ^
-                        -projectPath "%WORKSPACE%" ^
+            %UNITY_PATH% -batchmode ^
+                        -projectPath %WORKSPACE% ^
                         -executeMethod %UNITY_ASSET_METHOD% ^
                         -username %UNITY_USERNAME% ^
                         -password %UNITY_PASSWORD% ^
