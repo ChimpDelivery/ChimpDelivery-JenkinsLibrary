cd7e4822246308c26df926cd9c8c03a74a2ba9de
Emre Kovanci <kovanciemre@gmail.com>
2023-03-29 14:28:30 +0300

    Update crossShell.groovy

--- vars/crossShell.groovy
+++ vars/crossShell.groovy
@@ -1,15 +1,83 @@
-def call(Map config = [:])
+def call(labelStr, cmd, returnStatus = false, returnStdout = false, useWsl = false) 
 {
-    switch (env.OS)
+    if (isUnix()) 
     {
-        case 'WIN':
-            bat config.script
-        break;
-        case 'MAC':
-            sh config.script
-        break;
-        default:
-            sh config.script
-        break;
+        return sh(label: labelStr, script: cmd, returnStatus: returnStatus, returnStdout: returnStdout);
+    } 
+
+    return WindowFixReturn(bat(label: labelStr, script: WindowFixCmd(cmd, useWsl), returnStatus: returnStatus, returnStdout: returnStdout), returnStdout);
+}
+
+def ConsoleVar(varName) 
+{
+    if (isUnix()) 
+    {
+        return "\${${varName}}";
+    } 
+
+    return* "%${varName}%";
+}
+
+
+def WindowFixReturn(data, returnStdout) 
+{
+    if (returnStdout)
+    {
+        return data.trim().readLines().drop(1).join("\n");
     }
+
+    return data;
+}
+
+
+def WindowFixCmd(cmd, useWsl) 
+{
+    if (useWsl) 
+    {
+        return WindowsWslCommand(cmd);
+    }
+
+    return cmd;
+}
+
+ 
+
+def WindowsWslCommand(cmd) 
+{
+    return "wsl ${cmd}";
+}
+
+ 
+
+def WindowsEscapeVarPercentage(str) 
+{
+    if (isUnix()) 
+    {
+        return str;
+    }
+
+    *// If we want literal %*
+    return str.replace("%", "%%");
+}
+
+
+def ConsoleScriptExtension() 
+{
+    if (isUnix()) 
+    {
+        return ".sh";
+    } 
+    
+    return ".bat";
+}
+
+
+def ConsoleCommandDelimiter() 
+{
+    if (isUnix()) 
+    {
+        return ";";
+    }
+    
+    return "&";
 }
