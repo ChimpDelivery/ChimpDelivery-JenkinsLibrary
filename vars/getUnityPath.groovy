ef1e0afdbcb6d3d4eb9e0192620c5c961216255e
Emre Kovanci <kovanciemre@gmail.com>
2024-06-12 21:10:48 +0300

    add default case

--- vars/getUnityPath.groovy
+++ vars/getUnityPath.groovy
@@ -5,13 +5,17 @@ def call(Map config = [:])
         case 'WIN':
             return 'C:\\Program Files\\Unity\\Hub\\Editor\\' + config.unityVersion + '\\Editor\\Unity.exe';
         break;
-        
+
         case 'MAC':
             return "/Applications/Unity/Hub/Editor/${config.unityVersion}/Unity.app/Contents/MacOS/Unity";
         break;
-        
+
         case 'UBUNTU':
             return 'UBUNTU-OS';
+        break;
+
+        default:
+            return '/usr/bin/unity3d';
         break;
     }
 }
